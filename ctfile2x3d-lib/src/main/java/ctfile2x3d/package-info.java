28ddd2c9639d5496bcfaa42c8d6ebca9fa786fb1
Rafael Alcántara <rafael.alcantara@ebi.ac.uk>
2014-06-11 22:58:40 +0100

    Package info added.

---
+++ ctfile2x3d-lib/src/main/java/ctfile2x3d/package-info.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2014 rafa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * This package contains the main classes used as bridge between <a
+ * href="http://download.accelrys.com/freeware/ctfile-formats/ctfile-formats.zip">CTFiles</a>
+ * and their <a target="_top"
+ * href="http://www.web3d.org/realtime-3d/x3d/what-x3d">X3D</a> representation.
+ */
+package ctfile2x3d;
---
+++ x3d/src/main/java/org/web3d/x3d/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * JAXB-generated Java code for <a target="_top"
+ * href="http://www.web3d.org/realtime-3d/x3d/what-x3d">X3D</a>.
+ * <br>
+ * See also the <a target="_top"
+ * href="http://www.web3d.org/files/specifications/19775-1/V3.3/index.html">X3D
+ * specification</a>.
+ */
+package org.web3d.x3d;
